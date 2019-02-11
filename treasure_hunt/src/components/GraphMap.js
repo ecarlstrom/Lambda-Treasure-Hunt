b594af5fd260e422c5b87a6ddca923bb805fbece
Evan Carlstrom <evan.carlstrom@gmail.com>
2019-02-11 17:34:22 -0500

    added boilerplate for a GraphMap component that should be responsible for local storage

---
+++ treasure_hunt/src/components/GraphMap.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+class GraphMap extends Component {
+    // local storage here, will hold the travel path of the player
+};
+
+export default GraphMap;
