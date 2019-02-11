dab5b11d25438a4a39f73f3bc0a7f15d513ccc97
Evan Carlstrom <evan.carlstrom@gmail.com>
2019-02-11 17:48:13 -0500

    added axios dependency to project, built out more of GraphMap component along with initial state management

--- treasure_hunt/src/components/GraphMap.js
+++ treasure_hunt/src/components/GraphMap.js
@@ -3,6 +3,35 @@ import axios from 'axios';
 
 class GraphMap extends Component {
     // local storage here, will hold the travel path of the player
+    state = { travel: 0 };
+
+    findLocation = async() => {
+        try {
+            const res = await axios({
+                method: 'get',
+                url: 'https://lambda-treasure-hunt.herokuapp.com/api/adv/init',
+                headers: {
+                    Authorization: // link token here when .env setup is done
+                }
+            });
+            console.log(res.data);
+        } catch(err) {
+            console.error('An error was encountered. Please try again.');
+        }
+    }
+
+    handleClick = () => {
+        this.findLocation();
+    };
+
+    render() {
+        const { travel } = this.state;
+        return (
+            <div className = 'placeholder-div'>
+                <p>Progress bar (or whatever) placeholder</p>
+            </div>
+        );
+    }
 };
 
 export default GraphMap;
